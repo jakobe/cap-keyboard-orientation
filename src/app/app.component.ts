bcd6a236c9963d7a87e105daba379fbe4381fb9f
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-05-24 11:33:17 +0200

    Initial add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from "@ionic/angular";
+import { Plugins } from "@capacitor/core";
+const { SplashScreen } = Plugins;
+import { ScreenOrientation } from "@ionic-native/screen-orientation/ngx";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private orientation: ScreenOrientation
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+      if (this.platform.is("hybrid")) {
+        SplashScreen.hide();
+        this.orientation.lock(this.orientation.ORIENTATIONS.PORTRAIT_PRIMARY);
+      }
     });
   }
 }
